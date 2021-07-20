605d870b6fed51b73a71cfd526bd8f4b7144d3c2
tomastuser <tomas.tuser@ixperta.com>
2021-07-20 16:44:01 +0200

    drag styling added back

--- src/Example.js
+++ src/Example.js
@@ -1,13 +1,20 @@
 import { useSpring, animated } from '@react-spring/web';
 import { useGesture } from '@use-gesture/react';
 
+import styles from './styles.module.css';
+
 export const Example = () => {
   const [style, api] = useSpring(() => ({
+    x: 0,
+    y: 0,
     scale: 1,
     rotate: 0,
   }));
 
   const bind = useGesture({
+    // onDrag: ({ down, movement: [mx, my] }) => {
+    //   api.start({ x: down ? mx : 0, y: down ? my : 0 });
+    // },
     onPinch: ({ offset: [s, a] }) => {
       api.start({ scale: s, rotate: a });
     },
@@ -23,6 +30,7 @@ export const Example = () => {
         backgroundImage: 'url("./download.png")',
         backgroundSize: 'cover',
       }}
+      className={styles.drag}
     />
   );
 };
