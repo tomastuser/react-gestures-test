2c7bacc03f3c48ae4015ecf0cb98406a669974c5
tomastuser <tomas.tuser@ixperta.com>
2021-07-20 16:27:39 +0200

    initial commit

---
+++ src/Example.js
@@ -0,0 +1,36 @@
+import { useSpring, animated } from '@react-spring/web';
+import { useGesture } from '@use-gesture/react';
+
+import styles from './styles.module.css';
+
+export const Example = () => {
+  const [style, api] = useSpring(() => ({
+    x: 0,
+    y: 0,
+    scale: 1,
+    rotate: 0,
+  }));
+
+  const bind = useGesture({
+    onDrag: ({ down, movement: [mx, my] }) => {
+      api.start({ x: down ? mx : 0, y: down ? my : 0 });
+    },
+    onPinch: ({ offset: [s, a] }) => {
+      api.start({ scale: s, rotate: a });
+    },
+  });
+
+  return (
+    <animated.div
+      {...bind()}
+      style={{
+        ...style,
+        width: '500px',
+        height: '500px',
+        backgroundImage: 'url("./download.png")',
+        backgroundSize: 'cover',
+      }}
+      className={styles.drag}
+    />
+  );
+};
